1407cc986b4ea96eb59677aa96b022f6991f4229
min1974 <138650050+min1974@users.noreply.github.com>
2026-04-29 03:04:22 +0900

    modified:   Daily/4_28.py 	deleted:    Daily/EF1003_DLF_08_Data_Structure_List_Tuple_Set_Dictionary.pdf 	modified:   Personal/test.py

--- Daily/4_28.py
+++ Daily/4_28.py
@@ -27,22 +27,59 @@
 # print(b)
 
 # lab.1
-
+"""
 # 1 
-a = input('Enter a single line of integers: ').strip
+a = input('Enter a single line of integers: ').strip()
 b = a.split(',')
 sum = 0
 for i in b : 
     sum += int(i)
 print('sum is',sum)
 
+"""
 
-# 2
+# 2 
+
+"""
+
 CSV = input('Enter one-line CSV file: ').strip()
 house_value = CSV.split(';')
 list = []
+# print(CSV)
+# print(house_value)
 for i in house_value : 
-    list.append(i.split(',')) # [ [house, value] , [house,value] , ]
-for i in list : 
-    for j in (0,len(list)) : 
-        i[0] = list[j][0]
+    house_value_strip = i.split(',')
+    house_value_strip[0] = house_value_strip[0].strip()
+    house_value_strip[1] = house_value_strip[1].strip()
+    list.append(house_value_strip) # [ [house, value] , [house,value] , ]
+for i in range(len(list)) : 
+    list[i][1] = float(list[i][1])
+# print("list: {}".format(list))
+label_list = []
+value_list = []
+for i in range(len(list)) : 
+    if list[i][0] in label_list : 
+        continue
+    label_list.append(list[i][0])
+    total_value = 0
+    for j in range(len(list)) : 
+        if list[i][0] == list[j][0] : 
+            total_value += list[j][1]
+    value_list.append(total_value)
+
+finalText = ""
+for i in range(len(label_list)) :
+    finalText += f"{label_list[i]}:{value_list[i]}, "
+finalText = finalText[:-2]
+# print(label_list)
+print(finalText)
+
+"""
+
+#3.
+
+#4.
+
+#5.
+
+#6.
--- Personal/test.py
+++ Personal/test.py
@@ -1,1 +1,6 @@
-print(11)
+# a = "   c cc  c   c a "
+# print(a.strip("a            "))
+# print(type(" "))
+
+a = "   c cc  c   c a "
+print(a[:-4])
